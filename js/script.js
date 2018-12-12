4f871e2b113b2790ea2bfe03547ed7fad8515222
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-12 06:16:26 +0100

    Add markers to map

--- js/script.js
+++ js/script.js
@@ -4,24 +4,29 @@ let carouselData =
 					[
 						{	
 							title: 'Abguss-Sammlung Antiker Plastik',
-							image: 'images/Abguss-Sammlungmin.jpg'
+							image: 'images/Abguss-Sammlungmin.jpg',
+							coord: {lat: 52.455000, lng: 13.292135}
 						},
 						{	
 							title: 'Architekturmuseum der TU',
-							image: 'images/ArchitekturmuseumTUmin.jpg'
+							image: 'images/ArchitekturmuseumTUmin.jpg',
+							coord: {lat: 52.513648, lng: 13.324098}
 						},
 						{	
 							title: 'BStU-Bildungszentrum',
-							image: 'images/BStU-Bildungszentrummin.jpg'
+							image: 'images/BStU-Bildungszentrummin.jpg',
+							coord: {lat: 52.507788, lng: 13.387907}
 						},
 						{	
 							title: 'Deutscher Dom',
-							image: 'images/DeutscherDommin.jpg'
+							image: 'images/DeutscherDommin.jpg',
+							coord: {lat: 52.512750, lng: 13.392617}
 						},
 						{	
 							title: 'Deutsch-Russisches Museum',
-							image: 'images/Deutsch-RussischesMuseumin.jpg'
-						}						
+							image: 'images/Deutsch-RussischesMuseumin.jpg',
+							coord: {lat: 52.486312, lng: 13.539744}
+						}		
 					];
 
 let museumTemplate = document.getElementById('museum-template').innerHTML;
@@ -46,9 +51,17 @@ const flkty = new Flickity(carousel, {
 })();
 
 function initMap() {
-		map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 52.510349, lng: 13.430252},
+
+	let map = new google.maps.Map(document.getElementById('map'), {
+		center: carouselData[0].coord,
 		zoom: 10
 	});
+
+	for(let i=0; i<carouselData.length; i++) {
+		let marker = new google.maps.Marker({
+		    position: carouselData[i].coord,
+		    map: map
+		});
+	}
 }
 
